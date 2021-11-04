ff4c515873c6fe78007f3457dc54c60c0bc6d657
sahmanfaris <farissahman1@gmail.com>
2021-11-04 14:58:48 +0100

    implemented search feature

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import { useHistory } from 'react-router-dom'
+
+import './SearchBar.css'
+
+export default function Searchbar() {
+  const [term, setTerm] = useState('')
+  const history = useHistory()
+
+  const handleSubmit = e => {
+    e.preventDefault()
+
+    history.push(`/search?q=${term}`)
+  }
+
+  return (
+    <div className='searchbar'>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='search'>Search:</label>
+        <input
+          id='search'
+          type='text'
+          onChange={e => setTerm(e.target.value)}
+          required
+        />
+      </form>
+    </div>
+  )
+}
