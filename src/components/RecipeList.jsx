c11338a7479c115454da6ed4bf69b68431b01c59
@farissahman1 <farissahman1@gmail.com>
2021-11-05 23:31:57 +0100

    set up realtime listener for collection

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -1,23 +1,43 @@
-import { Link } from 'react-router-dom'
-import { useTheme } from '../hooks/useTheme'
+import { Link } from "react-router-dom";
+import { useTheme } from "../hooks/useTheme";
+import TrashCan from "../assets/trashcan.svg";
 
-import './RecipeList.css'
+import "./RecipeList.css";
+import { db } from "../firebase/config";
 
 export default function RecipeList({ recipes }) {
-  const { mode } = useTheme()
+  const { mode } = useTheme();
   if (recipes.length === 0) {
-    return <div className='error'>No recipes to load...</div>
+    return <div className="error">No recipes to load...</div>;
   }
+
+  const handleClick = (id) => {
+    db.collection("recipes")
+      .doc(id)
+      .delete()
+      .then(() => {
+        console.log("Document successfully deleted!");
+      })
+      .catch((error) => {
+        console.error("Error removing document: ", error);
+      });
+  };
   return (
-    <div className='recipe-list'>
-      {recipes.map(recipe => (
+    <div className="recipe-list">
+      {recipes.map((recipe) => (
         <div key={recipe.id} className={`card ${mode}`}>
           <h3>{recipe.title}</h3>
           <p>{recipe.cookingTime} to make.</p>
           <div>{recipe.method.substring(0, 100)}...</div>
           <Link to={`/recipes/${recipe.id}`}>Cook This</Link>
+          <img
+            src={TrashCan}
+            alt="trashcan"
+            className="delete"
+            onClick={() => handleClick(recipe.id)}
+          />
         </div>
       ))}
     </div>
-  )
+  );
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,42 +1,44 @@
-import { useEffect, useState } from 'react'
-import { db } from '../../firebase/config'
-import RecipeList from '../../components/RecipeList'
+import { useEffect, useState } from "react";
+import { db } from "../../firebase/config";
+import RecipeList from "../../components/RecipeList";
 
 // styles
-import './Home.css'
+import "./Home.css";
 
 export default function Home() {
-  const [data, setData] = useState(null)
-  const [isPending, setIsPending] = useState(false)
-  const [error, setError] = useState(false)
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState(false);
   useEffect(() => {
-    setIsPending(true)
+    setIsPending(true);
 
-    db.collection('recipes')
-      .get()
-      .then(snapshot => {
+    const unsubscribe = db.collection("recipes").onSnapshot(
+      (snapshot) => {
         if (snapshot.empty) {
-          setError('No recipes to load')
-          setIsPending(false)
+          setError("No recipes to load");
+          setIsPending(false);
         } else {
-          let results = []
-          snapshot.docs.forEach(doc => {
-            results.push({ ...doc.data(), id: doc.id })
-          })
-          setData(results)
-          setIsPending(false)
+          let results = [];
+          snapshot.docs.forEach((doc) => {
+            results.push({ ...doc.data(), id: doc.id });
+          });
+          setData(results);
+          setIsPending(false);
         }
-      })
-      .catch(err => {
-        setError(err.message)
-        setIsPending(false)
-      })
-  }, [])
+      },
+      (err) => {
+        setError(err.message);
+        setIsPending(false);
+      }
+    );
+    return () => unsubscribe();
+  }, []);
+
   return (
-    <div className='home'>
-      {error && <p className='error'>{error}</p>}
-      {isPending && <p className='loading'>Loading...</p>}
+    <div className="home">
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
       {data && <RecipeList recipes={data} />}
     </div>
-  )
+  );
 }
