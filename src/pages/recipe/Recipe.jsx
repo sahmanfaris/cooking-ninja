c643178f009545315f9036215e13413d3522c32e
@farissahman1 <farissahman1@gmail.com>
2021-11-05 23:40:52 +0100

    realtime update data

--- src/pages/recipe/Recipe.jsx
+++ src/pages/recipe/Recipe.jsx
@@ -1,54 +1,55 @@
-import { useEffect, useState } from 'react'
-import { useParams } from 'react-router'
-import { db } from '../../firebase/config'
-import { useTheme } from '../../hooks/useTheme'
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { db } from "../../firebase/config";
+import { useTheme } from "../../hooks/useTheme";
 
-import './Recipe.css'
+import "./Recipe.css";
 
 export default function Recipe() {
-  const [recipe, setRecipe] = useState(null)
-  const [isPending, setIsPending] = useState(false)
-  const [error, setError] = useState(false)
-  const { id } = useParams()
-  const { mode } = useTheme()
+  const [recipe, setRecipe] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState(false);
+  const { id } = useParams();
+  const { mode } = useTheme();
 
   useEffect(() => {
-    setIsPending(true)
-    var docRef = db.collection('recipes').doc(id)
+    setIsPending(true);
+    var docRef = db.collection("recipes").doc(id);
 
-    docRef
-      .get()
-      .then(doc => {
-        if (doc.exists) {
-          setRecipe(doc.data())
-          setIsPending(false)
-        } else {
-          setIsPending(false)
-          setError('No such recipe')
-        }
-      })
-      .catch(error => {
-        setError(error)
-        setIsPending(false)
-      })
-  }, [id])
+    const unsubscribe = docRef.onSnapshot((doc) => {
+      if (doc.exists) {
+        setRecipe(doc.data());
+        setIsPending(false);
+      } else {
+        setIsPending(false);
+        setError("No such recipe");
+      }
+    });
+
+    return () => unsubscribe();
+  }, [id]);
+
+  const handleClick = () => {
+    db.collection("recipes").doc(id).update({ title: "Something different" });
+  };
 
   return (
     <div className={`recipe ${mode}`}>
-      {error && <p className='error'>{error}</p>}
-      {isPending && <p className='loading'>Loading...</p>}
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
       {recipe && (
         <>
-          <h2 className='page-title'>{recipe.title}</h2>
+          <h2 className="page-title">{recipe.title}</h2>
           <p>Takes {recipe.cookingTime} to cook.</p>
           <ul>
-            {recipe.ingredients.map(ing => (
+            {recipe.ingredients.map((ing) => (
               <li key={ing}>{ing}</li>
             ))}
           </ul>
-          <p className='method'>{recipe.method}</p>
+          <p className="method">{recipe.method}</p>
+          <button onClick={handleClick}>Update me</button>
         </>
       )}
     </div>
-  )
+  );
 }
