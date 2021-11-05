716bd1cc3c86ca07fe3d183cfc96a4c3389d524c
sahmanfaris <farissahman1@gmail.com>
2021-11-05 09:01:50 +0100

    added color selector

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,13 @@
 import { Link } from 'react-router-dom'
+import { useTheme } from '../hooks/useTheme'
 
 import './Navbar.css'
 import Searchbar from './SearchBar'
 
 export default function Navbar() {
+  const { color } = useTheme()
   return (
-    <div className='navbar'>
+    <div className='navbar' style={{ background: color }}>
       <nav>
         <Link to='/' className='brand'>
           <h1>Cooking Ninja</h1>
